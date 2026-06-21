0d2c6dbadfe1d67330328f25439b8075c7dda66e
codesailor4 <codesailor4@gmail.com>
2026-06-21 05:21:18 +0000

    implemented the harden wallet

--- src/blockchain/blockchain.module.ts
+++ src/blockchain/blockchain.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlockchainService } from './blockchain.service';
+import { TransactionReconciliationService } from './services/transaction-reconciliation.service';
+import { TransactionVerificationService } from './services/transaction-verification.service';
+import { TransactionStatus } from './entities/transaction-status.entity';
 import { AnalyticsModule } from '../analytics/analytics.module';
 
 @Module({
-  imports: [AnalyticsModule],
-  providers: [BlockchainService],
-  exports: [BlockchainService],
+  imports: [TypeOrmModule.forFeature([TransactionStatus]), AnalyticsModule],
+  providers: [
+    BlockchainService,
+    TransactionReconciliationService,
+    TransactionVerificationService,
+  ],
+  exports: [
+    BlockchainService,
+    TransactionReconciliationService,
+    TransactionVerificationService,
+  ],
   controllers: [],
 })
 export class BlockchainModule {}
